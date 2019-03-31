5e599001d6a278eb10df9091ed79f61fe1c7185f
CamiloConcha <camiloaconchaa@gmail.com>
2019-03-31 00:16:41 -0500

    Added PropType Shape

--- WeatherApp/weather-app/src/components/WeatherData.js
+++ WeatherApp/weather-app/src/components/WeatherData.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
 import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from './../constants/weathers';
@@ -24,5 +25,13 @@ const WeatherData = ({
       <WeatherExtraInfo humidity={humidity} wind={wind} />
     </div>
   );
+};
+WeatherData.propTypes = {
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired
+  })
 };
 export default WeatherData;
